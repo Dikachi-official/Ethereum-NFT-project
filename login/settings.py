f5429b78c1e69261748a8c2fb2b6e32810d7746e
Dikachi-official <nwokontadikachi@gmail.com>
2023-01-03 19:02:36 +0100

    Settings Modification

--- login/settings.py
+++ login/settings.py
@@ -151,8 +151,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT ="staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
